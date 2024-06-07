7a1bbb6c27857460e0bd07d773c7155f66b41100
Salcuni Giovanni <salcuni@PALEXPO-LT-GS.local>
2024-06-07 11:19:20 +0200

    ajout de la recuperation des snippets

--- src/app/action/action.tsx
+++ src/app/action/action.tsx
@@ -4,16 +4,21 @@ import { db } from "@/db"
 import { redirect } from "next/navigation";
 
 // Création de la fonction de creation de snippet
-export async function createSnippet(formData: FormData){
+export async function createSnippet(formData: FormData) {
     const title = formData.get('title') as string;
     const code = formData.get('code') as string;
 
     await db.snippet.create({
-        data:{
+        data: {
             title,
             code
         }
     });
 
     redirect("/")
+}
+
+// Création de la fonction de récupération des snippets
+export async function fetchSnippets() {
+    return await db.snippet.findMany();
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { fetchSnippets } from "./action/action";
+
+export default async function Home() {
+  const snippets = await fetchSnippets();
+
+  const renderSnippet = snippets.map(snippet => {
+    return (
+      <div key={snippet.id}>
+        Titre du snippet : {snippet.title}
+      </div>
+    )
+  })
+
   return (
     <div>
       <h1>Home page</h1>
+      <div>
+        {renderSnippet}
+      </div>
     </div>
   );
 }
